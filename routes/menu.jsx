57450b120e04388f57c6baac2ceffcecedb028ff
Taylor Y <tdy721@gmail.com>
2023-04-22 08:32:47 -0700

    the menu

--- routes/menu.jsx
+++ routes/menu.jsx
@@ -21,6 +21,94 @@ export default function Showcase(props) {
           <img src={asset(`/menu.webp`)} alt="the menu" />
         </section>
         <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <p>🍣 Raw Fish Ingredients</p>
+          <p>🌶️ This Dish is Spicy</p>
+          <p>MOST ITEMS CONTAIN SESAME SEEDS</p>
+          <p>INGREDIENTS DEPENDING ON AVAILABILITY</p>
+          <p>SUBSTITUTION COST EXTRA</p>
+        </section>
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Noodles</h2>
+          <p>
+            TEMPURA UDON or SOBA
+            <span class="float-right">12</span>
+          </p>
+          <p>
+            KITSUNE UDON or SOBA
+            <span class="float-right">9</span>
+          </p>
+          <p>
+            KAKIAGE UDON or SOBA
+            <span class="float-right">10</span>
+          </p>
+          <p>
+            Add 2pcs Inari
+            <span class="float-right">+2</span>
+          </p>
+          <p>
+            EXTRA SAUCES etc.
+            <span class="float-right">+1</span>
+          </p>
+        </section>
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Combo</h2>
+          <p class="text-sm">served with rice and green salad</p>
+          <p>
+            Match One Set of Main and Sushi/Side
+            <span class="float-right">15.5</span>
+          </p>
+          <div class="float-left">
+            <h3>Main</h3>
+            <ul>
+              <li>TERIYAKI CHICKEN</li>
+              <li>CHICKEN KATSU</li>
+              <li>AJI FRY</li>
+            </ul>
+          </div>
+
+          <div class="float-left pl-5">
+            <h3>Side</h3>
+            <ul>
+              <li>California roll (4pcs)</li>
+              <li>Spicy crab roll (4pcs)</li>
+              <li>🌶️ 🍣 Spicy tuna roll (4pcs)</li>
+              <li>Shrimp tempura roll (4pcs)</li>
+              <li>🍣 Salmon roll (4pcs)</li>
+              <li>Cucumber roll (6pcs)</li>
+              <li>Egg roll (2pcs)</li>
+              <li>Sesame ball (3pcs)</li>
+              <li>Inari (2pcs)</li>
+            </ul>
+          </div>
+          <div class="clear-both">
+          </div>
+          <p>
+            🍣 HAND ROLL SET (2 hand rolls, 2 egg rolls, salad, miso soup)
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            🍣 CHIRASHI SUSHI BOWL (served with miso soup)
+            <span class="float-right">18</span>
+          </p>
+          <p>
+            🍣 SALMON SUSHI BOWL (served with miso soup)
+            <span class="float-right">18</span>
+          </p>
+          <p>
+            🍣 SUSHI SAMPLER
+            <span class="float-right">13</span>
+          </p>
+          <p>
+            2pcs INARI SUSHI (fried tofu pouch)
+            <span class="float-right">3.5</span>
+          </p>
+          <p>
+            5pcs INARI SUSHI (fried tofu pouch)
+            <span class="float-right">6.5</span>
+          </p>
+        </section>
+
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
           <h2>Appetizer</h2>
           <p>
             🌶️ NINJA JALAPENO (4pcs) <span class="float-right">9.5</span>
@@ -114,11 +202,182 @@ export default function Showcase(props) {
           </p>
         </section>
         <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
-          <p>🍣 Raw Fish Ingredients</p>
-          <p>🌶️ This Dish is Spicy</p>
-          <p>MOST ITEMS CONTAIN SESAME SEEDS</p>
-          <p>INGREDIENTS DEPENDING ON AVAILABILITY</p>
-          <p>SUBSTITUTION COST EXTRA</p>
+          <h2>Nigiri Sushi (2pcs)</h2>
+          <p>
+            🍣 ALBACORE<span class="float-right">6.5</span>
+          </p>
+          <p>
+            UNAGI (fresh water eel)<span class="float-right">6.5</span>
+          </p>
+          <p>
+            🍣 SALMON<span class="float-right">6.5</span>
+          </p>
+          <p>
+            SHRIMP<span class="float-right">5.5</span>
+          </p>
+          <p>
+            🍣 TUNA<span class="float-right">6.5</span>
+          </p>
+          <p>
+            🍣 WHITE FISH<span class="float-right">5.5</span>
+          </p>
+        </section>
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Maki Sushi (8pcs)</h2>
+          <p>
+            🌶️ 🍣 SPICY TUNA or SPICY SALMON
+            <span class="float-right">10</span>
+          </p>
+          <p>
+            🍣 TUNA or SALMON
+            <span class="float-right">9</span>
+          </p>
+          <p>
+            🌶️🌶️ 🍣 LAVA (spicy imitation crab, jalapeño, spicy tuna or salmon)
+            <span class="float-right">12</span>
+          </p>
+          <p>
+            🍣 RAINBOW
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            🍣 ATLANTIC (avocado cream cheese on top)
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            🍣 POKE (seasoned tuna, avocado, spicy mayo green onion)
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            🍣 PHILADELPHIA (smoke salmon. cream cheese)
+            <span class="float-right">11</span>
+          </p>
+          <p>
+            🍣 ALASKA (smoke salmon, avocado)
+            <span class="float-right">11</span>
+          </p>
+          <p>
+            🌶️ 🍣 DYNAMITE (spicy crab roll + spicy salmon, scallop, 3 sauces)
+            <span class="float-right">14.5</span>
+          </p>
+          <p>
+            🌶️ 🍣 LAS VEGAS (spicy salmon, avocado, cream cheese, deep fry)
+            <span class="float-right">15</span>
+          </p>
+          <p>
+            CRUNCHY (shrimp tempura, avacado, eel sauce, spicy mayo, crunchies)
+            <span class="float-right">12</span>
+          </p>
+          <p>
+            CALIFORNIA
+            <span class="float-right">6.5</span>
+          </p>
+          <p>
+            🌶️ SPICY CRAB (spicy surimi, cucumber)
+            <span class="float-right">6.5</span>
+          </p>
+          <p>
+            DRAGON (shrimp tempura roll + eel avocado eel sauce)
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            DOUBLE SHRIMP
+            <span class="float-right">14</span>
+          </p>
+          <p>
+            SPIDER DELIGHT (fried soft shell crab, cream cheese, avocado, green
+            onion, masago)
+            <span class="float-right">14.5</span>
+          </p>
+          <p>
+            SPIDER (5pcs) (fried soft shell crab)
+            <span class="float-right">13.5</span>
+          </p>
+          <p>
+            CATERPILLAR (eel avocado, eel sauce)
+            <span class="float-right">13</span>
+          </p>
+          <p>
+            КАРРА (6pcs) (cucumber)
+            <span class="float-right">4</span>
+          </p>
+          <p>
+            AVOCADO
+            <span class="float-right">5.5</span>
+          </p>
+          <p>
+            VEGETABLE (7 veggies)
+            <span class="float-right">12</span>
+          </p>
+        </section>
+
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Sashimi</h2>
+          <p>
+            🍣 SALMON<span class="float-right">15</span>
+          </p>
+          <p>
+            🍣 TUNA<span class="float-right">15</span>
+          </p>
+          <p>
+            🍣 ALBACORE<span class="float-right">15</span>
+          </p>
+          <br />
+          <p>
+            🍣 Sashimi Sampler (tuna, salmon, white fish, 2pcs
+            ea.)<span class="float-right">15</span>
+          </p>
+          <p>
+            🍣 Sashimi Mori (tuna, salmon, white fish, albacore 3pcs ea. Served
+            with rice and miso soup)<span class="float-right">25</span>
+          </p>
+        </section>
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Party Platter</h2>
+          <p>Call at least 2 hours in advance please</p>
+          <p>
+            MIXED ROLL PLATTER (37pcs)<br />
+            (1 shrimp tempura roll, 2 california rolls, 1 spicy crab roll, 5pcs
+            inari)
+            <span class="float-right">33</span>
+          </p>
+          <p>
+            🌶️ 🍣 Spicy Sushi Platter (40 pcs)<br />
+            (1 spicy tuna, 1 spicy salmon, 2 spicy crab rolls, 1 lava roll)
+            <span class="float-right">42</span>
+          </p>
+          <p>
+            🍣 Roll Platter (40pc)<br />
+            (2 california rolls, 1 shrimp tempura roll, 1 spicy tuna or spicy
+            salmon, 1 rainbow roll)
+            <span class="float-right">44</span>
+          </p>
+          <p>
+            VEGETABLE SUSHI PLATTER (41pcs)<br />
+            (1 vegetable roll, 1 avacado roll, 1 avacado + carrots roll, 5pcs
+            inari, 2 cucumber roll)
+            <span class="float-right">35</span>
+          </p>
+          <p>
+            🍣 NIGIRI & ROLL PLATER (39pcs)<br />
+            (tuna, salmon, albacore, white fish, shrimp, 3pcs each nigiri{" "}
+            <br />2 california rolls, 1 spicy tuna or spicy salmon roll)
+            <span class="float-right">58</span>
+          </p>
+        </section>
+        <section class="max-w-screen-lg mx-auto py-8 px(8) space-y-4 bg-white markdown">
+          <h2>Additional</h2>
+          <p>AVOCADO inside ($1.25)</p>
+          <p>AVOCADO on top ($2.50)</p>
+          <p>JALAPEÑO (75¢)</p>
+          <p>CUCUMBER (75¢)</p>
+          <p>MASAGO ($4)</p>
+          <p>SOY PAPER ($1.25)</p>
+          <p>GREEN ONIONS (75¢)</p>
+          <p>CRUNCHYS ($1)</p>
+          <p>CREAM CHEESE ($1)</p>
+          <p>EXTRA SAUCES ($1)</p>
+          <p>GINGER 8oz ($4)</p>
         </section>
       </Layout>
     </>
